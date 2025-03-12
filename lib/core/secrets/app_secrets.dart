99186dc86f59df41c7f7dc5606c50230046c71e0
seemantshekhar43 <seemantshekhar43@gmail.com>
2025-03-13 02:15:19 +0530

    chore: updated endpoint and project id

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,7 +1,7 @@
 class AppSecrets{
   AppSecrets._();
   static const String databaseId = '671c9eb40006bac28f76';
-  static const String projectId = '671c9e3f000d89af3cf8';
-  static const String endPoint = 'https://afeb-2405-201-a40a-387d-5984-e8aa-4aa3-21ea.ngrok-free.app/v1';
+  static const String projectId = '67d1ed82003579c30ddd';
+  static const String endPoint = 'https://regularly-enabled-satyr.ngrok-free.app/v1';
 
 }
